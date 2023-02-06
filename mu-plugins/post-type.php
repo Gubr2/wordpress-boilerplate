9558400644712b9d37a0f65f49ab9b543ef06e67
gubr2 <gubrica.a@gmail.com>
2023-02-06 12:58:12 +0100

    update

---
+++ mu-plugins/post-type.php
@@ -0,0 +1,23 @@
+<?php 
+
+// CUSTOM POST TYPES
+
+function post_types() {
+
+  // ---> Boilerplate
+
+  register_post_type('boilerplate', array(
+    'rewrite' => array(
+      'slug' => 'boilerplate'
+    ),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Boilerplate'
+    ),
+    'menu_icon' => 'dashicons-list-view',
+    'show_in_rest' => true,
+  ));
+}
+
+?>
